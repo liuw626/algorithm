38e4e9d70d451881ad1e519b72617f4bf58abec4
godric <shaoyun@pinduoduo.com>
2021-11-15 19:36:32 +0800

    update

--- src/com/godric/v21_11/algo/C07_BSAwesome.java
+++ src/com/godric/v21_11/algo/C07_BSAwesome.java
@@ -3,7 +3,7 @@ package com.godric.v21_11.algo;
 /**
  * 局部最小值问题: 在一个先递减后递增的数组中找到极小值点
  */
-public class C06_BSAwesome {
+public class C07_BSAwesome {
 
     public static int getLessIndex(int[] arr) {
         int left = 1;
