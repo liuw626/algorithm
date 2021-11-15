6e6fca2dcb3a9f598739c539c8ecd488417cd439
godric <shaoyun@pinduoduo.com>
2021-11-15 20:52:44 +0800

    KM, N

---
+++ src/com/godric/v21_11/algo/C09_KAndM_N.java
@@ -0,0 +1,12 @@
+package com.godric.v21_11.algo;
+
+public class C09_KAndM_N {
+
+    /**
+     * 在数组arr中, 只有一种数出现了k次, 其他都出现了m次, 返回这个数
+     */
+    public static int onlyKTimes(int[] arr, int k, int m) {
+        return -1;
+    }
+
+}
